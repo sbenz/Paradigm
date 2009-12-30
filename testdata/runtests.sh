5b6a68d592dae440161b8d0122921fa0ac1138c3
Charles Vaske <cvaske@soe.ucsc.edu>
2009-12-30 18:06:26 -0500

    updated testing to include test of previous node-split bug fix

--- testdata/runtests.sh
+++ testdata/runtests.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-echo Testing node splitting, should take less than a minute
-./pathwaytab2daifg needs_split.pathway.tab needs_split.cfg  \
-    | diff needs_split.out - || exit 1
+set -o pipefail
+
+echo Testing node splitting [1/2], should take less than a minute
+../pathwaytab2daifg needs_split_1.pathway.tab needs_split_1.cfg  \
+    | diff needs_split_1.out - || exit 1
+
+echo Testing node splitting [2/2], should take less than a minute
+../pathwaytab2daifg needs_split_2.pathway.tab needs_split_2.cfg  \
+    | diff needs_split_2.out - || exit 1
 
 echo Testing inference, should take less than a minute
-../hgFactorGraph -c noem.cfg -p small_pid_66_pathway.tab -b small_pid_66 \
+/usr/bin/time ../hgFactorGraph -c noem.cfg -p small_pid_66_pathway.tab -b small_pid_66 \
     | python ../helperScripts/diffSwarmFiles.py noem.cfg.out -\
     | diff - /dev/null \
     || exit 1
 
 echo Testing EM, should take approximately five minutes
-../hgFactorGraph -c em_simple.cfg -p small_pid_66_pathway.tab -b small_pid_66 \
-    python ../helperScripts/diffSwarmFiles.py em_simple.cfg.out -\
-    diff - /dev/null || exit 1
-
+/usr/bin/time ../hgFactorGraph -c em_simple.cfg -p small_pid_66_pathway.tab -b small_pid_66 \
+    | python ../helperScripts/diffSwarmFiles.py em_simple.cfg.out -\
+    | diff - /dev/null \
+    || exit 1
